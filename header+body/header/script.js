b3131b25b9d32cf84356a2c0070b40b10d618681
Thnh8052 <vuthanh250804@gmail.com>
2024-10-22 15:30:40 +0700

    Add files via upload

---
+++ header+body/header/script.js
@@ -0,0 +1,31 @@
+$('.card-list').slick({
+  infinite: false,
+  speed: 1,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
