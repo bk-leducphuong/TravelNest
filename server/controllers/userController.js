542fcbf558fe8d2e5b17fcb6fd8d6a623c2eef3a
Le Duc Phuong <phuongtroc2004@gmail.com>
2024-12-03 00:26:59 +0700

    user information settings

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -25,7 +25,125 @@ const getBookingInformation = async (req, res) => {
     }
 }
 
+// edit user information controllers
+const editName = async (req, res) => {
+    try {
+        const {name} = req.body;
+        const userId = req.session.user.user_id;
+        const query = `UPDATE users SET full_name = ? WHERE user_id = ?`;
+        await queryAsync(query, [name, userId]);
+        res.status(200).json({ success: true });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+const editDisplayName = async (req, res) => {
+    try {
+        const {displayName} = req.body;
+        const userId = req.session.user.user_id;
+        const query = `UPDATE users SET username = ? WHERE user_id = ?`;
+        await queryAsync(query, [displayName, userId]);
+        res.status(200).json({ success: true });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+const editEmail = async (req, res) => {
+    try {
+        const {email} = req.body;
+        const userId = req.session.user.user_id;
+        const query = `UPDATE users SET email = ? WHERE user_id = ?`;
+        await queryAsync(query, [email, userId]);
+        res.status(200).json({ success: true });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+const editPhoneNumber = async (req, res) => {
+    try {
+        const {phoneNumber} = req.body;
+        const userId = req.session.user.user_id;
+        const query = `UPDATE users SET phone_number = ? WHERE user_id = ?`;
+        await queryAsync(query, [phoneNumber, userId]);
+        res.status(200).json({ success: true });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+const editDateOfBirth = async (req, res) => {
+    try {
+        const {dateOfBirth} = req.body;
+        const userId = req.session.user.user_id;
+        const query = `UPDATE users SET date_of_birth = ? WHERE user_id = ?`;
+        await queryAsync(query, [dateOfBirth, userId]);
+        res.status(200).json({ success: true });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+const editAddress = async (req, res) => {
+    try {
+        const {address} = req.body;
+        const userId = req.session.user.user_id;
+        const query = `UPDATE users SET address = ? WHERE user_id = ?`;
+        await queryAsync(query, [address, userId]);
+        res.status(200).json({ success: true });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+const editNationality = async (req, res) => {
+    try {
+        const {nationality} = req.body;
+        const userId = req.session.user.user_id;
+        const query = `UPDATE users SET country = ? WHERE user_id = ?`;
+        await queryAsync(query, [nationality, userId]);
+        res.status(200).json({ success: true });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+const editGender = async (req, res) => {
+    try {
+        const {gender} = req.body;
+        const userId = req.session.user.user_id;
+        const query = `UPDATE users SET gender = ? WHERE user_id = ?`;
+        await queryAsync(query, [gender, userId]);
+        res.status(200).json({ success: true });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+const editAvatar = async (req, res) => {
+    try {
+        const {avatar} = req.body;
+        const userId = req.session.user.user_id;
+        const query = `UPDATE users SET profile_picture_url = ? WHERE user_id = ?`;
+        await queryAsync(query, [avatar, userId]);
+        res.status(200).json({ success: true });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
 module.exports = {
     getUserInformation,
-    getBookingInformation
+    getBookingInformation,
+    editName,
+    editDisplayName,
+    editEmail,
+    editPhoneNumber,
+    editDateOfBirth,
+    editAddress,
+    editNationality,
+    editGender,
+    editAvatar
 }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const {isUserAuthenticated} = require('../middlewares/sessionAuth');
-const { getUserInformation, getBookingInformation } = require( '../controllers/userController.js');
+const { getUserInformation, getBookingInformation, editName, editDisplayName, editEmail, editPhoneNumber, editDateOfBirth, editAddress, editNationality, editGender, editAvatar } = require( '../controllers/userController.js');
 const router = express.Router();
 
 // root route: /api/user
 router.use(isUserAuthenticated);
 
 router.get('/get-user-information', getUserInformation);
-router.post('/me', );
+// Route to edit user information
+router.post('/edit-name', editName);
+router.post('/edit-display-name', editDisplayName);
+router.post('/edit-email', editEmail);
+router.post('/edit-phone-number', editPhoneNumber);
+router.post('/edit-date-of-birth', editDateOfBirth);
+router.post('/edit-address', editAddress);
+router.post('/edit-nationality', editNationality);
+router.post('/edit-gender', editGender);
+router.post('/edit-avatar', editAvatar);
 
 // Route to get booking information of user
 router.post('/booking/get-booking-information', getBookingInformation);
